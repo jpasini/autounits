a90b3dbec9d609fa96dad1a80bc17ca1f873560b
Jose Miguel Pasini <josemiguelpasini@gmail.com>
2012-02-26 15:08:39 -0500

    First complete version. Runs.

--- pypace.py
+++ pypace.py
@@ -17,7 +17,7 @@ def mph_to_mps(mph):
     '''Miles/hour to meters/sec'''
     return mph*miles_to_meters(1)/hours_to_seconds(1)
 
-def pace(mph):
+def one_mile_pace(mph):
     '''Time per mile.'''
     return secs_for_distance(miles_to_meters(1), mph_to_mps(mph))
     
@@ -37,49 +37,46 @@ def secs_to_string(secs):
     hours = int(secs/3600)
     secs = secs - hours*3600
     mins = int(secs/60)
-    secs = secs - mins*60
+    secs = int(secs - mins*60)
     result = ""
     if hours > 0:
         result = result + str(hours) + ":"
     result = result + '{0:02d}:{1:02d}'.format(mins,secs)
     return result
 
-
+def print_table_row(mph):
+    '''Print mph and pace for 1 mile, 5k, 10k, half, and full marathon.'''
+    pace1m = secs_to_string(one_mile_pace(mph))
+    pace5k = secs_to_string(secs_for_5k(mph))
+    pace10k = secs_to_string(secs_for_10k(mph))
+    pacehalf = secs_to_string(secs_for_half_marathon(mph))
+    pacefull = secs_to_string(secs_for_marathon(mph))
+    print '{0:.1f} & {1} & {2} & {3} & {4} & {5} \\\\'.format(mph, pace1m, pace5k, pace10k, pacehalf, pacefull)
+    
+def print_table():
+    mph_table = [5 + i/5 for i in range(31)]
+    for mph in mph_table:
+        print_table_row(mph)
     
     
-    
-def print_problem(pair):
-    '''Given a pair [i,j], print a problem.'''
-    print pair[0], ' \\times ', pair[1], '&= \\qquad & '
-    
-def print_latex_header(num_problems,time_for_problems):
+def print_latex_header():
     print '''
 \\documentclass[12pt]{article}
 \\usepackage{amsmath}
+\\usepackage{fullpage}
 \\begin{document}
-\\begin{Huge}\n'''
-    print num_problems, 'problems,', time_for_problems, 'minutes:\n'
-    print '\\begin{align*}'
+\\begin{large}
+\\thispagestyle{empty}
+\\begin{tabular}{rrrrrr}
+mph & 1mi & 5k & 10k & half & full \\\\'''
 
 def print_latex_footer():
-    print '''\\end{align*}
-\\end{Huge}
+    print '''\\end{tabular}
+\\end{large}
 \\end{document}'''
     
 if __name__ == "__main__":
     import sys
-    if len(sys.argv) != 3:
-        print '\nUsage: python times_tables.py num_problems> time_for_problems > output.tex\n'
-        exit(0)
-    num_problems = int(sys.argv[1])
-    time_for_problems = int(sys.argv[2])
-    pairs = create_all_pairs(9,12)
-    import random
-    my_problems = random.sample(pairs,num_problems)
-    print_latex_header(num_problems,time_for_problems)
-    for i in range(len(my_problems)):
-        if i % 3 == 0:
-            print '\\\\ '
-        print_problem(my_problems[i])
-        
+    print_latex_header()
+    print_table()
     print_latex_footer()
