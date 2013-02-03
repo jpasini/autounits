4b4b1b01d8d4ea30e24235e9bb24b39e7152195d
Jose Miguel Pasini <josemiguelpasini@gmail.com>
2013-02-03 11:07:43 -0500

    Implemented creation from params or from other dimension. Implemented equality and difference check. Three tests pass.

--- dimension.py
+++ dimension.py
@@ -10,5 +10,36 @@ class IncompatibleDimensionsError(DimensionError): pass
 
 
 class Dimension(object):
-    """Class describing dimensions (length, time, mass, and derivative dimensions.)"""
-        
+    """Class describing dimensions: length, time, etc. and derivative dimensions."""
+    
+    def __init__(self, M = 0, L = 0, T = 0, Q = 0, Theta = 0):
+        """Can be initialized like this:
+        Dimension(L = 1, T = -2) using named arguments,
+        Dimension(d) using another dimension
+        Dimension("L/T") using a string."""
+        # check the first argument in case it's a different type
+        if type(M) == type(self): # it's a dimension
+            # To do: check that there are no other arguments?
+            self.M = M.M
+            self.L = M.L
+            self.T = M.T
+            self.Q = M.Q
+            self.Theta = M.Theta
+        elif type(M) == str:
+            pass
+        else:
+            self.M = M
+            self.L = L
+            self.T = T
+            self.Q = Q
+            self.Theta = Theta
+            
+    def __eq__(self, other):
+        """Check for equality."""
+        return self.M == other.M and self.L == other.L \
+            and self.T == other.T and self.Q == other.Q and self.Theta == other.Theta
+    
+    def __ne__(self, other):
+        """Check for difference."""
+        return self.M != other.M or self.L != other.L \
+            or self.T != other.T or self.Q != other.Q or self.Theta != other.Theta
